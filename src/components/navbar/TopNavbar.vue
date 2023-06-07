f42a277dafc4ab3a22c2332349fbc5e273b57e38
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-06-08 03:10:06 +0800

    Refactor TopNavbar.vue for authentication purpose

--- src/components/navbar/TopNavbar.vue
+++ src/components/navbar/TopNavbar.vue
@@ -1,6 +1,5 @@
 <template>
     <v-app-bar
-        color="orange-darken-4"
         class="position-fixed"
         :image="image"
     >
@@ -10,18 +9,33 @@
             />
         </template>
 
-        <template v-slot:prepend>
-            <v-app-bar-nav-icon />
+        <template v-if="authStore.isAuthenticated" v-slot:prepend>
+            <v-app-bar-nav-icon/>
         </template>
 
         <v-app-bar-title class="pally">{{ store.app.brand }}</v-app-bar-title>
 
-        <v-spacer />
-
+        <v-spacer/>
 
         <label class="switch me-3">
-            <span class="sun"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g fill="#ffd43b"><circle r="5" cy="12" cx="12"></circle><path d="m21 13h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm-17 0h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm13.66-5.66a1 1 0 0 1 -.66-.29 1 1 0 0 1 0-1.41l.71-.71a1 1 0 1 1 1.41 1.41l-.71.71a1 1 0 0 1 -.75.29zm-12.02 12.02a1 1 0 0 1 -.71-.29 1 1 0 0 1 0-1.41l.71-.66a1 1 0 0 1 1.41 1.41l-.71.71a1 1 0 0 1 -.7.24zm6.36-14.36a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm0 17a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm-5.66-14.66a1 1 0 0 1 -.7-.29l-.71-.71a1 1 0 0 1 1.41-1.41l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.29zm12.02 12.02a1 1 0 0 1 -.7-.29l-.66-.71a1 1 0 0 1 1.36-1.36l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.24z"></path></g></svg></span>
-            <span class="moon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="m223.5 32c-123.5 0-223.5 100.3-223.5 224s100 224 223.5 224c60.6 0 115.5-24.2 155.8-63.4 5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6-96.9 0-175.5-78.8-175.5-176 0-65.8 36-123.1 89.3-153.3 6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"></path></svg></span>
+            <span class="sun">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <g fill="#ffd43b">
+                        <circle r="5" cy="12" cx="12">
+                        </circle>
+                        <path
+                            d="m21 13h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm-17 0h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm13.66-5.66a1 1 0 0 1 -.66-.29 1 1 0 0 1 0-1.41l.71-.71a1 1 0 1 1 1.41 1.41l-.71.71a1 1 0 0 1 -.75.29zm-12.02 12.02a1 1 0 0 1 -.71-.29 1 1 0 0 1 0-1.41l.71-.66a1 1 0 0 1 1.41 1.41l-.71.71a1 1 0 0 1 -.7.24zm6.36-14.36a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm0 17a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm-5.66-14.66a1 1 0 0 1 -.7-.29l-.71-.71a1 1 0 0 1 1.41-1.41l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.29zm12.02 12.02a1 1 0 0 1 -.7-.29l-.66-.71a1 1 0 0 1 1.36-1.36l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.24z">
+                        </path>
+                    </g>
+                </svg>
+            </span>
+            <span class="moon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                    <path
+                        d="m223.5 32c-123.5 0-223.5 100.3-223.5 224s100 224 223.5 224c60.6 0 115.5-24.2 155.8-63.4 5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6-96.9 0-175.5-78.8-175.5-176 0-65.8 36-123.1 89.3-153.3 6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z">
+                    </path>
+                </svg>
+            </span>
             <input
                 type="checkbox"
                 class="input"
@@ -64,7 +78,7 @@
                                 variant="text"
                             >
                                 <v-icon icon="mdi-logout"/>
-                                Log Out
+                                Sign Out
                             </v-list-item>
                         </v-list>
                     </v-menu>
@@ -72,9 +86,9 @@
                 <v-card>
                     <v-card-title class="bg-orange-accent-2">
                         <v-icon id="remind">mdi-alert-circle</v-icon>
-                        Confirm Logout
+                        Confirm Sign Out
                     </v-card-title>
-                    <v-card-text>Are you sure you want to log out?</v-card-text>
+                    <v-card-text>Are you sure you want to sign out?</v-card-text>
                     <v-card-actions>
                         <v-spacer></v-spacer>
                         <v-btn
@@ -91,15 +105,15 @@
                             @click="signOut"
                             :loading="signingOut"
                         >
-                            Log Out
+                            Sign Out
                         </v-btn>
                     </v-card-actions>
                 </v-card>
             </v-dialog>
         </template>
         <template v-else>
-            <SignIn />
-            <SignUp />
+            <SignIn/>
+            <SignUp/>
         </template>
 
     </v-app-bar>
@@ -112,7 +126,7 @@ import {useStore} from "@/stores";
 import SignIn from "@/components/dialogs/SignInDialog.vue";
 import {useAuthStore} from "@/stores/store-auth";
 import {useRouter} from "vue-router";
-import { useTheme } from "vuetify";
+import {useTheme} from "vuetify";
 
 // data
 const dialog = ref(false);
@@ -224,9 +238,10 @@ onMounted(() => {
     height: 24px;
 }
 
-/* .switch:hover */.sun svg {
-                       animation: rotate 15s linear infinite;
-                   }
+/* .switch:hover */
+.sun svg {
+    animation: rotate 15s linear infinite;
+}
 
 @keyframes rotate {
 
@@ -239,9 +254,10 @@ onMounted(() => {
     }
 }
 
-/* .switch:hover */.moon svg {
-                       animation: tilt 5s linear infinite;
-                   }
+/* .switch:hover */
+.moon svg {
+    animation: tilt 5s linear infinite;
+}
 
 @keyframes tilt {
 
